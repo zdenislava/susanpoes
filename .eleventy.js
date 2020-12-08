b762b0841b9ddbba4bedb07946b79cef91e35538
Zdenka Navarikova <navarikova@seznam.cz>
2020-12-08 14:41:48 +0100

    první commit

---
+++ .eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+
+	// Výchozí výstupní složka: _site
+
+	// Zkopírovat `images/` do `_site/images`
+	eleventyConfig.addPassthroughCopy("images");
+
+	// Zkopírovat `css/` to `_site/css/`
+	eleventyConfig.addPassthroughCopy("css");
+
+
+  return {
+    // možné formáty šablon
+    templateFormats: ["njk","html", "liquid", "md"],
+
+    // jako šablonovací jazyk zvolíme Nunjucks
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+  }
+
+};
